6ae2a99c9871a65f32a6e9fe68d316cd4b412415
Joe <zsklari11@gmail.com>
2021-02-16 11:39:18 +0100

    debugged

--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class HeroService {
+  getAllHeroes(): import("../model/hero").Hero[] {
+    throw new Error('Method not implemented.');
+  }
 
   constructor() { }
 }
